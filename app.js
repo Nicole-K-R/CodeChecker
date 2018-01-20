e4a6b72ad41f439b816cb5e893c16fb06d0e20ec
Nicole Rosario <nkrosario@rogers.com>
2018-01-19 21:22:01 -0500

    Added dependencies for mongoose and nodemon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
+const mongoose = require('mongoose');
 
 // GET method route
 app.get('/', function (req, res) {
