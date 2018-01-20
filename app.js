b01060325732c31bf5f2ead7c57039cc04252ce4
Nicole Rosario <nkrosario@rogers.com>
2018-01-20 06:59:44 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 // ******* MongoDB Requirements *******//
-const mongoose = require('mongoose');
-const MongoClient = require('mongodb').MongoClient;
+// const mongoose = require('mongoose');
+// const MongoClient = require('mongodb').MongoClient;
 // ******* GitHub API Requirements *******//
 var request = require('request');
 var path = require('path');
@@ -16,9 +16,10 @@ var read = require('file-reader');
 // ******* Other Requirements *******//
 const assert = require('assert');
 const fs = require('fs');
+var isDirectory = require('is-directory');
+var glob = require('glob');
 // ******* Variable(s) *******//
 var folder = 'uploads/';
-
 
 // Receives data as a JSON object (should be identical for each language)
     // Called by checkPythonFormatting
@@ -28,13 +29,11 @@ var scoring = function(data){
     return true; // Replace with score
 }
 
-
 // Converts the text files to JSON objects and calculates a score
     // Calls scoring & called by checkPythonFormatting
 var textToJSONPython = function(fileName){
     var file1 = fileName + '1.txt';
     var file2 = fileName + '2.txt';
-    console.log('File1: ' + file1);
     // Read first file
     var content1 = read(file1);
     console.log(content1);
@@ -49,18 +48,26 @@ var textToJSONPython = function(fileName){
 }
 
 // Moves all python files from cctmp folder and moves them to python folder
+    // ****** Need to check folders too ******** //
     // Called by checkPythonFormatting
 var movePythonFiles = function (){
-    console.log('In movePythonFiles');
-    fs.readdirSync('cctmp').forEach(function(file) {
-        if (path.extname('.py')){
-            console.log('Moving: ' + file.name);
-            file.path = __dirname + './python';
-            console.log(file.path);
-        }
-    });
-}
+    var isPython = false;
+    var directories = [];
+    var pythonFiles = [];
+    var numDirectories = 0;
+    var first = true;
+    
 
+    // Check if it is a folder or a file (****** Not working, need to go through the files and go into directories to check all files)*****
+    
+    
+    console.log('Python files: ' + pythonFiles);
+    console.log('# Python files: ' + pythonFiles.length);
+    console.log('directories: ' + directories);
+    console.log('# directories: ' + directories.length);
+    console.log('isPython: ' + isPython);  
+    return isPython;
+}
 
 // Check python formatting (checks pep8 on cctmp folder and checks for formatting errors in the .py files and
 // stores errors in text files in the uploads directory)
@@ -69,26 +76,26 @@ var checkPythonFormatting = function(repoDetails){
     // Make directory and clone files from the repo into it
     const mkdir = spawnSync('mkdir', ['./cctmp']);
     const clone = spawnSync('git', ['clone', repoDetails.clone_url, './cctmp']);
-    movePythonFiles();
+    var hasPython = movePythonFiles();
     // Delete files from cctmp folder
-    // fs.readdirSync('cctmp').forEach(function(file) {
-    //     console.log('Deleting: ' + file);
-    //     fs.unlinkSync(file);
-    // });
     const rmdir = spawnSync('rm', ['-r', './cctmp']);
     var repoName = repoDetails.name;
-    
-    // Install pycodestyle and change directory to uploads folder
-    cmd.run('pip install pycodestyle');
-    cmd.run('cd python');
-        // Run pycodestyle on the python files and save it to <repoName>.txt 
-        // (first displays errors and solutions, second displays number of each errors)
-        // pycodestyle --show-source --show-pep8 <folder> > uploads/test.txt (1)
-        // pycodestyle --statistics -qq <folder> > uploads/test.txt (2)
-    cmd.run('pycodestyle --show-source --show-pep8 python > ./uploads/' + repoName + '1.txt');
-    cmd.run('pycodestyle --statistics -qq  python > ./uploads/' + repoName + '2.txt');
-    // Call function to turn error text files to json to send back to front-end
-    return textToJSONPython(repoName); // Send score
+    if (hasPython){
+        // Install pycodestyle and change directory to uploads folder
+        cmd.run('pip install pycodestyle');
+        cmd.run('cd python');
+            // Run pycodestyle on the python files and save it to <repoName>.txt 
+            // (first displays errors and solutions, second displays number of each errors)
+            // pycodestyle --show-source --show-pep8 <folder> > uploads/test.txt (1)
+            // pycodestyle --statistics -qq <folder> > uploads/test.txt (2)
+        cmd.run('pycodestyle --show-source --show-pep8 python > ./uploads/' + repoName + '1.txt');
+        cmd.run('pycodestyle --statistics -qq  python > ./uploads/' + repoName + '2.txt');
+        // Call function to turn error text files to json to send back to front-end
+        return textToJSONPython(repoName); // Send score
+    }
+    else{
+        return -1; // Return -1 if there are no python files in the repo
+    }
 }
 
 // Gets all GitHub repos under a given username
@@ -117,6 +124,7 @@ var getFiles = function(userName){
     var scorePy =[];
     getAllRepos(userName, function(repos) {
         for(var i = 0; i < repos.length; i++) {
+            console.log('--------------------------------- ' + (i+1) + ' : ' + repos[i].name + ' ---------------------------------');
             scorePy.push(repos[i].name, checkPythonFormatting(repos[i]));
         }
     });
@@ -124,7 +132,9 @@ var getFiles = function(userName){
     var score = 0;
     console.log('ScorePy: ' + scorePy);
     for (var i = 0; i < scorePy.length; i ++){
-        score += scorePy[i][1];
+        if (score !== -1){
+            score += scorePy[i][1];
+        }
     }
     return score;
 }
@@ -145,7 +155,6 @@ app.get('/', function (req, res) {
 app.get('/:userName', function (req, res) {
     var userName = req.params.userName
     var url = 'https://github.com/' + userName;
-ç
     // Download files from github repos to uploads folder
     getFiles(userName); 
     res.sendFile('views/index.html' , { root : __dirname});
