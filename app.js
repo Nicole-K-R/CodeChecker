4fbad28ef79bbc272b0356cd627fdc83ba6976f1
Nicole Rosario <nkrosario@rogers.com>
2018-01-19 21:03:10 -0500

    Added mysql dependency and linked app.js to index.html on root route

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 
 // GET method route
 app.get('/', function (req, res) {
-    res.send('Get root route')
-  })
+    // res.send('Get root route');
+    res.sendFile('views/index.html' , { root : __dirname});});
   
   // POST method route
-  app.post('/', function (req, res) {
+app.post('/', function (req, res) {
     res.send('POST root route')
-  })
+});
 
 app.listen(7000, function(){
     console.log('Example app listening on port 7000!');
