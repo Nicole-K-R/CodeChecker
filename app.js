490d55e59b43d01b6ac8669fa7ddb1a49a885cde
Nicole Rosario <nkrosario@rogers.com>
2018-01-20 16:34:08 -0500

    Testing

--- app.js
+++ app.js
@@ -27,34 +27,33 @@ var score = require('./score');
 // }
 
 // Delete files in a specific folder
-var deleteAllFilesInFolder = function(folder){
-    fs.readdirSync(folder).forEach(function (file) {
+var deleteAllFilesInFolder = function(){
+    fs.readdirSync('python').forEach(function (file) {
         fs.unlinkSync(path.join(__dirname, 'python/' + file));
     });
 }
 
 // Converts the text files to JSON objects and calculates a score
     // Called by checkPythonFormatting
-var textToJSONPython = async function(fileName, extensionValue = '.py'){
-    var textFiles = [];
-    fs.readdirSync(folder).forEach(function (file) {
-        textFiles.push(file);
-    });
-    var scoreValue = score.scoreProfile(textFiles);
-    console.log(scoreValue);
+var getScores = async function(repoName){
+    var scoreRepo = 0;
+    scoreRepo = await score.scoreRepo(repoName + '2.txt');
+    console.log('Hello: ' + scoreRepo);
+    return scoreRepo; // Number between -inifinity and 100
 }
+getScores('keller-mark');
 
+// Walks through files and directories and makes a list of all files with a certain extension
+    // Called by movePythonFiles and itself
 var walkThroughFiles = function(dir, extension, filelist) {
     var files = fs.readdirSync(dir);
     filelist = filelist || [],
     extension = extension || '';
-    var allFilesList = []; //***************//
     files.forEach(function(file) {
         if (fs.statSync(path.join(dir, file)).isDirectory()) {
             filelist = walkThroughFiles(path.join(dir, file), extension, filelist);
         }
         else {
-            allFilesList.push(file);
             if (path.extname(file) === extension){
                 filelist.push(file);
             }
@@ -65,7 +64,7 @@ var walkThroughFiles = function(dir, extension, filelist) {
 
 
 // Moves all python files from cctmp folder and moves them to python folder
-    // Called by checkPythonFormatting
+    // Called by checkPythonFormatting (***** WORKS *****)
 var movePythonFiles = function (){
     var pythonFiles = walkThroughFiles('cctmp', '.py');
     for (var i = 0; i < pythonFiles.length; i ++){
@@ -76,7 +75,7 @@ var movePythonFiles = function (){
 // Check python formatting (checks pep8 on cctmp folder and checks for formatting errors in the .py files and
 // stores errors in text files in the uploads directory)
     // Calls textToJSONPython & called by getFiles
-var checkPythonFormatting = function(repoDetails){
+var checkPythonFormatting = async function(repoDetails){
     // Make directory and clone files from the repo into it
     const mkdir = spawnSync('mkdir', ['./cctmp']);
     const clone = spawnSync('git', ['clone', repoDetails.clone_url, './cctmp']);
@@ -98,12 +97,16 @@ var checkPythonFormatting = function(repoDetails){
     // Delete python files
     deleteAllFilesInFolder('python');
     // Call function to turn error text files to json to send back to front-end
-    return textToJSONPython(repoName); // Send score
+    var scores = getScores('./uploads/' + repoName); // Send score
+    return scores; // Format number
 }
+
+// checkPythonFormatting();
+// console.log('* was called');
 
 // Gets all GitHub repos under a given username
     // Calls checkPythonFormatting and getAllRepos & called by express route
-var getAllRepos = function(username, callback) {
+var getAllRepos = async function(username, callback) {
     //request({ url: 'https://api.github.com/users/' + username + '/repos', headers: githubHeaders }, function(error, response, body) {
 	//https://api.github.com/search/repositories?q=+user:daniel-e+language:python&sort=stars&order=desc	
       request({ url: 'https://api.github.com/search/repositories?q=+user:' + username + '+language:python&sort=stars&order=desc', headers: githubHeaders }, function(error, response, body) {
@@ -126,11 +129,11 @@ var getAllRepos = function(username, callback) {
 
 // Gets repos from GitHub API
     // Calls checkPythonFormatting and getAllRepos & called by express route
-var getFiles = function(userName){
+var getFiles = async function(userName){
     var scorePy =[];
     getAllRepos(userName, function(repos) {
         for(var i = 0; i < repos.length; i++) {
-            scorePy.push(repos[i].name, checkPythonFormatting(repos[i]));
+            scorePy.push([repos[i].name, checkPythonFormatting(repos[i])]);
         }
     });
     //Calculate overall score and return
@@ -147,27 +150,29 @@ var getFiles = function(userName){
     return JSON.stringify(obj);
 }
 
-// ------------------------------------------ Express Routing ------------------------------------------ //
-// View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+getFiles('keller-mark');
 
-// GET method route
-app.get('/', function (req, res) {
-    // res.send('Get root route');
-    res.sendFile('views/index.html' , { root : __dirname});});
+// // ------------------------------------------ Express Routing ------------------------------------------ //
+// // View engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'html');
+
+// // GET method route
+// app.get('/', function (req, res) {
+//     // res.send('Get root route');
+//     res.sendFile('views/index.html' , { root : __dirname});});
   
-// POST method route (sendurl)
-//app.post('/sendurl', function (req, res) {
-app.get('/:userName', async function (req, res) {
-    var userName = req.params.userName
-    var url = 'https://github.com/' + userName;
-    // Download files from github repos to uploads folder
-    res.send(getFiles(userName)); 
-});
+// // POST method route (sendurl)
+// //app.post('/sendurl', function (req, res) {
+// app.get('/:userName', async function (req, res) {
+//     var userName = req.params.userName
+//     var url = 'https://github.com/' + userName;
+//     // Download files from github repos to uploads folder
+//     res.send(getFiles(userName)); 
+// });
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.listen(7000, function(){
-    console.log('Example app listening on port 7000!');
-});
+// app.listen(7000, function(){
+//     console.log('Example app listening on port 7000!');
+// });
