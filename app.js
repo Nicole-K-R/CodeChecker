a40ade156597115f2417d07a2717f42a01dbd755
Nicole Rosario <nkrosario@rogers.com>
2018-01-19 20:37:03 -0500

    Added front-end.js and style.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// GET method route
+app.get('/', function (req, res) {
+    res.send('Get root route')
+  })
+  
+  // POST method route
+  app.post('/', function (req, res) {
+    res.send('POST root route')
+  })
 
-app.listen(7000, () => console.log('Example app listening on port 7000!'));
+app.listen(7000, function(){
+    console.log('Example app listening on port 7000!');
+});
