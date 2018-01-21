35f683ccf81d96b1cd861b846484fa5a912aab98
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-01-21 01:41:23 -0500

    added more scoring details

--- score.js
+++ score.js
@@ -1,7 +1,7 @@
 const db = require('./db');
 const fs = require('fs');
 
-var weightScale = 1;
+var weightScale = 10;
 
 var regexWeight = function(line, ruleObjects) {
   for(var i = 0; i < ruleObjects.length; i++) {
@@ -18,16 +18,36 @@ var regexWeight = function(line, ruleObjects) {
     }
   }
   return 0; //no matching regex found, 0 weight
-}
+};
 
 var getCategoryName = function(line, categoryObjects) {
   for(var i = 0; i < categoryObjects.length; i++) {
     var regex = new RegExp(categoryObjects[i].regex_match);
     if(line.match(regex) != null) {
-      return categoryObjects.name;
+      return categoryObjects[i].name;
     }
   }
   return null;
+};
+
+var getMostFrequentLines = function(all_lines) {
+  for(var i = 0; i < all_lines.length; i++) {
+    var line = all_lines[i];
+    var regex = new RegExp('(\\d*).*[A-Z]\\d*(.*)');
+    var matches = regex.exec(line);
+    var messageFrequency = []; // map number to message
+    if(matches != null && matches.length == 2) {
+      messageFrequency.push([ parseInt(matches[0]), matches[1] ]);
+    }
+  }
+  messageFrequency.sort(function(a, b) {
+    return a[0] - b[0];
+  });
+  if(messageFrequency.length >= 5) {
+    return messageFrequency.slice(0, 5);
+  } else {
+    return messageFrequency; //returns array of 'tuple'-type arrays [ [22, "message"], ...]
+  }
 };
 
 var scoreRepo = async function(filepath, extensionValue = '.py') {
@@ -37,42 +57,32 @@ var scoreRepo = async function(filepath, extensionValue = '.py') {
     var langObject = await db.Language.findOne({ '_id': langObjectID });
     var ruleObjects = await db.StyleRule.find({ 'language_id': langObjectID });
     var categoryObjects = await db.StyleCategory.find({ 'language_id': langObjectID });
+    
     var categories = {}; // map category name to category score
 
     // set initial repo score to 100
     var score = 100.0;
     var lines = fs.readFileSync(filepath).toString().split('\n');
+
+    
     // iterate over each line, deduct weighted amounts based on the style rule
     for(var i = 0; i < lines.length; i++) {
       var lineWeight = regexWeight(lines[i], ruleObjects);
       score -= lineWeight;
       var categoryName = getCategoryName(lines[i], categoryObjects);
-      if(!categories[categoryName]) {
-        categories[categoryName] = lineWeight;
-      } else {
-        categories[categoryName] += lineWeight;
+      if(categoryName != null) {
+        if(!categories[categoryName]) {
+          categories[categoryName] = lineWeight;
+        } else {
+          categories[categoryName] += lineWeight;
+        }
       }
 
     }
-    return score;
+
+    score = Number((score).toFixed(0));
+    var messageFrequency = getMostFrequentLines(lines);
+    var result = { 'score': score, 'categories': categories, 'frequent': messageFrequency };
+    return result;
 };
 exports.scoreRepo = scoreRepo;
-
-var scoreProfile = async function(all_filepaths, extensionValue = '.py') {
-  var profileScore = 0;
-  var numRepos = 0;
-  for(var i = 0; i < all_filepaths.length; i++) {
-    var repoScore = await scoreRepo(all_filepaths[i], extensionValue);
-    profileScore += repoScore;
-    numRepos++;
-  }
-  // average all repos together to compute profileScore
-  if(numRepos == 0) {
-    return 0;
-  } else {
-    profileScore = profileScore / numRepos;
-  }
-  return profileScore;
-}
-exports.scoreProfile = scoreProfile;
-
